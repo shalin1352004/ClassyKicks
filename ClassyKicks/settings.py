c4ff76f7bbee161c39f937067a396e7eae06e8e2
Shalin <shalinmodi60@gmail.com>
2025-07-03 19:19:01 +0530

    third launch

--- ClassyKicks/settings.py
+++ ClassyKicks/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8dbcf)ikcxh*2dsh!!q5m87**)+&o4oh^%3s@qt&ruoh1!0wf*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
