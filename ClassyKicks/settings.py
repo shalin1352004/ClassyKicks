ef30a9017b575ee705761b1fe7920f68c0ba012b
Shalin <shalinmodi60@gmail.com>
2025-07-04 12:12:18 +0530

    final launch

--- ClassyKicks/settings.py
+++ ClassyKicks/settings.py
@@ -130,9 +130,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
