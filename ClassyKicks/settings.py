1ccc43447a8b02aabe46a1f3f02480b5a887b66e
Shalin <shalinmodi60@gmail.com>
2025-07-04 12:52:48 +0530

    final launch

--- ClassyKicks/settings.py
+++ ClassyKicks/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
     },
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
